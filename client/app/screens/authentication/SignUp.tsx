022c22672c1bb2813843a859c16f231486801a22
Ella <ellalala.mc@gmail.com>
2025-07-12 07:41:57 +0800

    feat: :sparkles: Create sign up screen

--- client/app/screens/authentication/SignUp.tsx
+++ client/app/screens/authentication/SignUp.tsx
@@ -1,10 +1,33 @@
-import React from "react";
-import { View, Text } from "react-native";
+import { Stack } from "expo-router";
+import React, { useState } from "react";
+import { View, Text, Platform, SafeAreaView, StatusBar } from "react-native";
+import SignUpForm1 from "./components/SignUpForm1";
 
 export default function SignUpScreen() {
+  const [step, setStep] = useState<number>(1);
+
   return (
-    <View>
-      <Text> sup</Text>
-    </View>
+    <>
+      <Stack.Screen options={{ headerShown: false }} />
+      <SafeAreaView
+        className="flex-1 bg-background"
+        style={{
+          paddingVertical: Platform.OS === "android" ? StatusBar.currentHeight : 0,
+        }}
+      >
+        <StatusBar backgroundColor="white" />
+        <View className="flex-1 px-12 bg-background">
+          <View className="flex flex-col pt-12">
+            <Text className="text-text font-sora-bold text-4xl">Create</Text>
+            <Text className="text-text font-sora-bold text-4xl">account</Text>
+          </View>
+          <View>
+            <Text className="text-text font-dm-sans py-10 tracking-widest">{step} of 3</Text>
+            {step === 1 && <SignUpForm1 />}
+            {/* Additional steps to be added */}
+          </View>
+        </View>
+      </SafeAreaView>
+    </>
   )
 }
