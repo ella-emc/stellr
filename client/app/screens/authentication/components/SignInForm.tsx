1839c52b57bfee5d152faa46344d904ecc2cc705
Ella <ellalala.mc@gmail.com>
2025-07-11 19:26:15 +0800

    feat: :sparkles: Add toggle password visibility

--- client/app/screens/authentication/components/SignInForm.tsx
+++ client/app/screens/authentication/components/SignInForm.tsx
@@ -1,11 +1,18 @@
-import React from "react";
+import React, { useState } from "react";
 import { Controller, useForm } from "react-hook-form";
-import { Text, TextInput, View } from "react-native";
+import { Text, TextInput, View, TouchableOpacity, Button, Pressable } from "react-native";
 import { FontAwesomeIcon } from "@fortawesome/react-native-fontawesome";
 import { faUserAstronaut } from "@fortawesome/free-solid-svg-icons/faUserAstronaut";
+import { faEye, faEyeSlash } from "@fortawesome/free-solid-svg-icons";
 
 export default function SignInForm() {
   const { control, handleSubmit, formState: { errors } } = useForm();
+  const [showPassword, setShowPassword] = useState<boolean>(false);
+
+  const onSubmit = (data: any) => {
+    console.log("Form submitted with data:", data);
+    // Submission logic goes here
+  };
 
   return (
     <View className="flex flex-col">
@@ -17,19 +24,59 @@ export default function SignInForm() {
           <View className="flex-col gap-y-2">   
             <Text className="text-accent font-dmSans">Username</Text>       
             <TextInput
-              className="bg-backgroundLight text-white p-4 rounded-xl mb-4 font-dmSans relative"
-              placeholder="Username"
+              className="bg-backgroundLight text-white p-4 rounded-xl mb-8 font-dmSans relative"
+              placeholder="Your username"
               placeholderTextColor="#FFFFFF40"
               value={value}
               onChangeText={onChange}
             />
-            <View className="absolute right-4 top-1/2 -translate-y-1/4">
-              <FontAwesomeIcon icon={faUserAstronaut} size={18} style={{ color: "#FFFFFF", opacity: 0.5 }}/>
+            <View className="absolute right-4 top-1/2 -translate-y-1/2">
+              <FontAwesomeIcon 
+                icon={faUserAstronaut} 
+                size={18} 
+                style={{ color: "#FFFFFF", opacity: 0.5 }}
+              />
             </View>
           </View>
         )}
       />
       {errors.username && <Text className="text-red-500">Username is required</Text>}
+
+      <Controller 
+        control={control}
+        name="password"
+        rules={{ required: true }}
+        render={({ field: { onChange, value } }) => (
+          <View className="flex-col gap-y-2">   
+            <Text className="text-accent font-dmSans">Password</Text>       
+            <TextInput
+              className="bg-backgroundLight text-white p-4 rounded-xl mb-4 font-dmSans relative"
+              placeholder="Your password"
+              placeholderTextColor="#FFFFFF40"
+              value={value}
+              onChangeText={onChange}
+              secureTextEntry={!showPassword}
+            />
+            <View className="absolute right-4 top-1/2 -translate-y-1/4">
+              <TouchableOpacity onPress={() => setShowPassword(!showPassword)}>
+                <FontAwesomeIcon 
+                  icon={showPassword ? faEyeSlash : faEye} 
+                  size={18} 
+                  style={{ color: "#FFFFFF", opacity: 0.5 }}
+                />
+              </TouchableOpacity>
+            </View>
+          </View>
+        )}
+      />
+      {errors.password && <Text className="text-red-500">Username is required</Text>}
+
+      <Pressable 
+        className="flex items-center justify-center bg-accent mt-9 p-4 rounded-xl font-dmSans mt-4"
+        onPress={handleSubmit(onSubmit)}
+      >
+        <Text className="text-background font-sora">Sign in</Text>
+      </Pressable>
     </View>
   )
 }
