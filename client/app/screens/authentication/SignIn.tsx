bb851a87a20fc1cb3fa1baa8e345144ba9e34a33
Ella <ellalala.mc@gmail.com>
2025-07-11 19:25:03 +0800

    style: :lipstick: Change paddings

--- client/app/screens/authentication/SignIn.tsx
+++ client/app/screens/authentication/SignIn.tsx
@@ -1,14 +1,12 @@
 import React from "react";
 import { SafeAreaView, View, Text, StatusBar, Platform } from "react-native";
-import { Stack } from "expo-router";
+import { Link, Stack } from "expo-router";
 import { ImageBackground } from "expo-image";
 import { LinearGradient } from "expo-linear-gradient";
 import { useForm } from "react-hook-form";
 import SignInForm from "./components/SignInForm";
 
 export default function SignInScreen() {
-  const { control, handleSubmit, formState: { errors } } = useForm();
-
   return (
     <>
       <Stack.Screen options={{ headerShown: false }} />
@@ -35,13 +33,17 @@ export default function SignInScreen() {
             className="absolute w-full h-full"
           />
 
-          <View className="flex-1 items-center justify-center gap-y-6">
-            <Text className="text-white font-dmSans text-sm tracking-[0.2em]">WELCOME TO</Text>
-            <Text className="text-white font-sora text-5xl tracking-[0.2em]">Stellr</Text>
-          </View>
-
-          <View className="px-9">
-            <SignInForm />
+          <View className="flex-1 pb-28 pt-14">
+            <View className="flex items-center justify-center gap-y-6 py-16">
+              <Text className="text-white font-dmSans text-sm tracking-[0.2em]">WELCOME TO</Text>
+              <Text className="text-white font-sora text-5xl tracking-[0.2em]">Stellr</Text>
+            </View>
+            <View className="px-12">
+              <SignInForm />
+              <Text className="text-text font-dmSans mt-8">
+                Don't have an account yet? <Link href="/screens/authentication/SignUp" className="text-accent">Sign up</Link>
+              </Text>
+            </View>
           </View>
         </ImageBackground>
       </SafeAreaView>
