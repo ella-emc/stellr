a68048478b8d66ad2407b17205ea137f90acac59
Ella <ellalala.mc@gmail.com>
2025-07-13 14:29:30 +0800

    feat: :sparkles: Finish first page of sign up form

--- client/app/screens/authentication/components/SignUpForm1.tsx
+++ client/app/screens/authentication/components/SignUpForm1.tsx
@@ -3,14 +3,19 @@ import { faEnvelope, faEye, faEyeSlash, faUserAstronaut } from "@fortawesome/fre
 import { FontAwesomeIcon } from "@fortawesome/react-native-fontawesome";
 import React, { useState } from "react";
 import { Controller, useForm } from "react-hook-form";
-import { Text, TextInput, View } from "react-native";
+import { Pressable, Text, TextInput, View } from "react-native";
 
 export default function SignUpForm1() {
-  const { control, handleSubmit, formState: { errors } } = useForm();
+  const { control, handleSubmit, formState: { errors }, getValues } = useForm();
 
   // State to handle password visibility
   const [showPassword, setShowPassword] = useState<boolean>(false);
   const [showConfirmPassword, setShowConfirmPassword] = useState<boolean>(false);
+
+  const onSubmit = (data: any) => {
+    console.log("Form submitted with data:", data);
+    // Submission logic goes here
+  };
 
   return (
     <View className="flex-col gap-y-4">
@@ -42,6 +47,24 @@ export default function SignUpForm1() {
         secureTextEntry={!showPassword}
         toggleVisibility={() => setShowPassword(!showPassword)}
       />
+
+      <CustomInput 
+        control={control}
+        name="confirmPassword"
+        rules={{ required: true, validate: (value: string) => value === getValues("password") || "Passwords do not match" }}
+        placeholder="Confirm your password"
+        label="Confirm Password"
+        icon={showConfirmPassword ? faEyeSlash : faEye}
+        secureTextEntry={!showConfirmPassword}
+        toggleVisibility={() => setShowConfirmPassword(!showConfirmPassword)}
+      />
+
+      <Pressable 
+        className="flex items-center justify-center bg-primary mt-9 p-4 rounded-xl font-dm-sans mt-4"
+        onPress={handleSubmit(onSubmit)}
+      >
+        <Text className="text-background font-sora-bold">Next</Text>
+      </Pressable>
     </View>
   )
 }
