d76eda1304c44fc7b8bdc2feca11001a41d10ab2
Ella <ellalala.mc@gmail.com>
2025-07-13 14:05:42 +0800

    refactor: :recycle: Import reusable input

--- client/app/screens/authentication/components/SignUpForm1.tsx
+++ client/app/screens/authentication/components/SignUpForm1.tsx
@@ -1,62 +1,44 @@
-import { faEnvelope, faUserAstronaut } from "@fortawesome/free-solid-svg-icons";
+import CustomInput from "@/components/forms/CustomInput";
+import { faEnvelope, faEye, faEyeSlash, faUserAstronaut } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-native-fontawesome";
-import React from "react";
+import React, { useState } from "react";
 import { Controller, useForm } from "react-hook-form";
 import { Text, TextInput, View } from "react-native";
 
 export default function SignUpForm1() {
   const { control, handleSubmit, formState: { errors } } = useForm();
 
+  // State to handle password visibility
+  const [showPassword, setShowPassword] = useState<boolean>(false);
+  const [showConfirmPassword, setShowConfirmPassword] = useState<boolean>(false);
+
   return (
     <View>
-      <Controller 
+      <CustomInput 
         control={control}
-        name="email"
+        name="Email"
         rules={{ required: true }}
-        render={({ field: { onChange, value } }) => (
-          <View className="flex-col gap-y-2">   
-            <Text className="text-text/80 font-dm-sans">Email</Text>       
-            <TextInput
-              className="bg-backgroundLight text-white p-4 rounded-xl mb-1 font-dm-sans relative"
-              placeholder="example@domain.com"
-              placeholderTextColor="#E5E5E540"
-              value={value}
-              onChangeText={onChange}
-            />
-            <View className="absolute right-4 top-1/2">
-              <FontAwesomeIcon 
-                icon={faEnvelope} 
-                size={18} 
-                style={{ color: "#FFFFFF", opacity: 0.5 }}
-              />
-            </View>
-          </View>
-        )}
+        placeholder="example@domain.com"
+        label="Email"
+        icon={faEnvelope}
       />
 
-      <Controller 
+      <CustomInput 
         control={control}
         name="username"
         rules={{ required: true }}
-        render={({ field: { onChange, value } }) => (
-          <View className="flex-col gap-y-2">   
-            <Text className="text-accent font-dm-sans">Username</Text>       
-            <TextInput
-              className="bg-backgroundLight text-white p-4 rounded-xl mb-1 font-dm-sans relative"
-              placeholder="Your username"
-              placeholderTextColor="#E5E5E540"
-              value={value}
-              onChangeText={onChange}
-            />
-            <View className="absolute right-4 top-1/2">
-              <FontAwesomeIcon 
-                icon={faUserAstronaut} 
-                size={18} 
-                style={{ color: "#FFFFFF", opacity: 0.5 }}
-              />
-            </View>
-          </View>
-        )}
+        placeholder="Your username"
+        label="Username"
+        icon={faUserAstronaut}
+      />
+
+      <CustomInput 
+        control={control}
+        name="password"
+        rules={{ required: true, minLength: 8 }}
+        placeholder="Your password"
+        label="Create Password"
+        icon={showPassword ? faEyeSlash : faEye}
       />
     </View>
   )
