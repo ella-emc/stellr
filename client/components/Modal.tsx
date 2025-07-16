ff896803785794f4649e945b34d1ce9d7b374aef
Ella <ellalala.mc@gmail.com>
2025-07-16 20:42:04 +0800

    feat: :construction: Set up custom reusable modal component

---
+++ client/components/Modal.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import { View, Text, Pressable } from "react-native";
+
+type ModalProps = {
+  // Prop definition here
+}
+
+export default function Modal() {
+  return (
+    <View>
+      <Text>Sample Modal</Text>
+    </View>
+  )
+}
