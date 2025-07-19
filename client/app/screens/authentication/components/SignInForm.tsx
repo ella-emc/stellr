671dc71f62e91dffa1524922784da549df9f6516
Ella <ellalala.mc@gmail.com>
2025-07-19 17:49:55 +0800

    refactor: :recycle: Use custom input component

--- client/app/screens/authentication/components/SignInForm.tsx
+++ client/app/screens/authentication/components/SignInForm.tsx
@@ -4,6 +4,7 @@ import { Text, TextInput, View, TouchableOpacity, Button, Pressable } from "reac
 import { FontAwesomeIcon } from "@fortawesome/react-native-fontawesome";
 import { faUserAstronaut } from "@fortawesome/free-solid-svg-icons/faUserAstronaut";
 import { faEye, faEyeSlash } from "@fortawesome/free-solid-svg-icons";
+import CustomInput from "@/components/forms/CustomInput";
 
 export default function SignInForm() {
   const { control, handleSubmit, formState: { errors } } = useForm();
@@ -15,64 +16,31 @@ export default function SignInForm() {
   };
 
   return (
-    <View className="flex flex-col">
-      <Controller 
+    <View className="flex flex-col gap-y-6">
+      <CustomInput
         control={control}
         name="username"
+        label="Username"
+        icon={faUserAstronaut}
+        placeholder="Your username"
         rules={{ required: true }}
-        render={({ field: { onChange, value } }) => (
-          <View className="flex-col gap-y-2">   
-            <Text className="text-text/80 font-dm-sans">Username</Text>       
-            <TextInput
-              className="bg-backgroundLight text-white p-4 rounded-xl mb-1 font-dm-sans relative"
-              placeholder="Your username"
-              placeholderTextColor="#E5E5E540"
-              value={value}
-              onChangeText={onChange}
-            />
-            <View className="absolute right-4 top-1/2">
-              <FontAwesomeIcon 
-                icon={faUserAstronaut} 
-                size={18} 
-                style={{ color: "#FFFFFF", opacity: 0.5 }}
-              />
-            </View>
-          </View>
-        )}
+        labelTextColor="text-text"
       />
-      {errors.username && <Text className="text-accent font-dm-sans">Invalid username</Text>}
 
-      <Controller 
+      <CustomInput
         control={control}
         name="password"
+        label="Password"
+        icon={showPassword ? faEyeSlash : faEye}
+        placeholder="Your password"
+        secureTextEntry={!showPassword}
+        toggleVisibility={() => setShowPassword(!showPassword)}
         rules={{ required: true }}
-        render={({ field: { onChange, value } }) => (
-          <View className="flex-col gap-y-2 mt-6">   
-            <Text className="text-text/80 font-dm-sans">Password</Text>       
-            <TextInput
-              className="bg-backgroundLight text-white p-4 rounded-xl font-dm-sans relative"
-              placeholder="Your password"
-              placeholderTextColor="#E5E5E540"
-              value={value}
-              onChangeText={onChange}
-              secureTextEntry={!showPassword}
-            />
-            <View className="absolute right-4 top-1/2">
-              <TouchableOpacity onPress={() => setShowPassword(!showPassword)}>
-                <FontAwesomeIcon 
-                  icon={showPassword ? faEyeSlash : faEye} 
-                  size={18} 
-                  style={{ color: "#FFFFFF", opacity: 0.5 }}
-                />
-              </TouchableOpacity>
-            </View>
-          </View>
-        )}
+        labelTextColor="text-text"
       />
-      {errors.password && <Text className="font-dm-sans text-accent">Invalid password</Text>}
 
       <Pressable 
-        className="flex items-center justify-center bg-primary mt-9 p-4 rounded-xl font-dm-sans mt-4"
+        className="flex items-center justify-center bg-primary mt-4 p-4 rounded-xl font-dm-sans mt-4"
         onPress={handleSubmit(onSubmit)}
       >
         <Text className="text-background font-sora-bold">Sign in</Text>
